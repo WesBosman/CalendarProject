5dfac37629e40a7428f11843341a1d005a630f7c
Wes <wes@EGR-MACLAB-35.local>
2016-05-16 13:50:30 -0400

    Appointments and tasks are both working for the home screen next thing to do would be selection

--- CreativeCalendar/HomeViewController.swift
+++ CreativeCalendar/HomeViewController.swift
@@ -15,10 +15,10 @@ class HomeViewController: UIViewController , UITableViewDataSource, UITableViewD
     @IBOutlet weak var appointmentViewTable: UITableView!
     @IBOutlet weak var taskViewTable: UITableView!
     @IBOutlet weak var journalViewBox: UITextView!
-    var secondArray: [AppointmentItem] = []
-    var thirdArray: [TaskItem] = []
-    let secondCell = "AppointmentHomeCell"
-    let thirdCell = "TaskHomeCell"
+    var appointmentArray: [AppointmentItem] = []
+    var taskArray: [TaskItem] = []
+    let appointmentCellID = "AppointmentHomeCell"
+    let taskCellID = "TaskHomeCell"
     
     @IBOutlet weak var uncheckedTask: UIImageView!
     @IBOutlet weak var labelTask: UILabel!
@@ -34,12 +34,16 @@ class HomeViewController: UIViewController , UITableViewDataSource, UITableViewD
         taskLabel.textColor = UIColor.whiteColor()
         journalLabel.text = "Journal"
         journalLabel.textColor = UIColor.whiteColor()
+        
         // Get all appointments and tasks.
-        secondArray = AppointmentItemList.sharedInstance.allItems()
-        thirdArray = TaskItemList.sharedInstance.allTasks()
+        appointmentArray = AppointmentItemList.sharedInstance.allItems()
+        taskArray = TaskItemList.sharedInstance.allTasks()
+        
         // Set this class up to be the delegate for the two different table views
         self.taskViewTable.delegate = self
         self.taskViewTable.dataSource = self
+        self.appointmentViewTable.delegate = self
+        self.appointmentViewTable.dataSource = self
         //self.tasks = TaskItemList.sharedInstance.allTasks()
         
         
@@ -53,38 +57,37 @@ class HomeViewController: UIViewController , UITableViewDataSource, UITableViewD
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if tableView == taskViewTable{
-            return thirdArray.count
+        if tableView == appointmentViewTable{
+            return appointmentArray.count
         }
         else{
-            return secondArray.count
+            return taskArray.count
         }
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        if tableView == taskViewTable{
-            let task = thirdArray[indexPath.row] as TaskItem
-            let cell = taskViewTable.dequeueReusableCellWithIdentifier(thirdCell, forIndexPath: indexPath) as! HomeTaskCell
-            //let picImage = UIImage(named: "uncheckbox")
-            //cell.uncheckedImage = UIImageView(image: picImage)
-            print("Task Title: \(task.taskTitle)\n")
-            print("Task Info: \(task.taskInfo)\n")
-            cell.homeTaskTitle.text = task.taskTitle
-            cell.homeTaskInfo.text = task.taskInfo
-            return cell
-        }
-        else{
-            let appointment = secondArray[indexPath.row] as AppointmentItem
-            let cell = appointmentViewTable.dequeueReusableCellWithIdentifier(secondCell, forIndexPath: indexPath) as! HomeAppointmentCell
-            print("Appointment Title: \(appointment.title)\n")
+        // fill the appointment table view cell and return it
+        if tableView == appointmentViewTable{
+            let appointment = appointmentArray[indexPath.row] as AppointmentItem
+            let appCell = appointmentViewTable.dequeueReusableCellWithIdentifier(appointmentCellID, forIndexPath: indexPath) as! HomeAppointmentCell
+            print("\nAppointment Title: \(appointment.title)\n")
             print("Appointment Start: \(appointment.startingTime)\n")
             print("Appointment End: \(appointment.endingTime)\n")
             print("Appointment Location: \(appointment.appLocation)\n")
             print("Appointment Additional Info: \(appointment.additionalInfo)\n")
-            cell.homeAppointmentTitle.text = appointment.title
-            cell.homeAppointmentSubtitle.text = "start: \(appointment.startingTime) end: \(appointment.endingTime) location: \(appointment.appLocation)"
-            return cell
+            appCell.homeAppointmentTitle.text = appointment.title
+            appCell.homeAppointmentSubtitle.text = "start: \(appointment.startingTime) end: \(appointment.endingTime) location: \(appointment.appLocation)"
+            return appCell
         }
+        
+        // Other wise fill the task table view cell and return it
+        let task = taskArray[indexPath.row] as TaskItem
+        let taskCell = taskViewTable.dequeueReusableCellWithIdentifier(taskCellID, forIndexPath: indexPath) as! HomeTaskCell
+        print("\nTask Title: \(task.taskTitle)\n")
+        print("Task Info: \(task.taskInfo)\n")
+        taskCell.homeTaskTitle.text = task.taskTitle
+        taskCell.homeTaskInfo.text = task.taskInfo
+        return taskCell
     }
     
 
